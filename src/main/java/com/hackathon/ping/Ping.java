dc60371ffc658fd2960bcbad506375f65d5adbd6
schristou88 <schristou88@gmail.com>
2012-09-25 18:05:46 -0500

    Initial commit

---
+++ src/main/java/com/hackathon/ping/Ping.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package main.java.com.hackathon.ping;
+
+/**
+ * @author schristou88
+ *
+ */
+public class Ping {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
