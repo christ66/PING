d5203b80a5beb5f61891da8ebcbd5bbbb44b8b61
Jedidiah Johnson <river226@gmail.com>
2012-10-02 13:08:56 -0500

    Set Layout Manager

--- src/main/java/com/hackathon/ping/PingGUI.java
+++ src/main/java/com/hackathon/ping/PingGUI.java
@@ -5,6 +5,7 @@
 package main.java.com.hackathon.ping;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -17,42 +18,27 @@ import java.awt.event.MouseListener;
 
 public class PingGUI {
 	JFrame frame;
-	JButton winex;
+	JPanel base;
+	JButton winex, winmax, winmin;
+	Box box;
 
 	public PingGUI() {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		int w = 900, h = 600;
 		int x = (dim.width-w)/2;
 		int y = (dim.height-h)/2;
-		frame = new JFrame();
+		frame = new JFrame("PING: PING is not Git");
 		frame.setUndecorated(true);
 		frame.setResizable(false);
 		frame.setBackground(Color.LIGHT_GRAY);
 		frame.setSize(w, h);
 		frame.setLocation(x, y);
-		frame.setTitle("PING: PING is not Git");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+		box = new Box(BoxLayout.X_AXIS);
+		buildWinOpArea();
+		frame.add(box);
 		
-		winex = new JButton("exit");
-		winex.setSize(20, 10);
-		winex.addMouseListener(new MouseListener(){
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) { System.exit(0);	}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) { /* NOTHING */ }
-
-			@Override
-			public void mouseExited(MouseEvent arg0) { /* NOTHING */ }
-
-			@Override
-			public void mousePressed(MouseEvent arg0) { /* NOTHING */ }
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) { /* NOTHING */ }
-			
-		});
-		frame.add(winex);
 		
 		
 		frame.addKeyListener(new KeyListener() {
@@ -76,6 +62,67 @@ public class PingGUI {
 		});
 		
 		frame.setVisible(true);
+	}
+	
+	private void buildWinOpArea() {
+		
+		winex = new JButton("exit");
+		winex.setSize(20, 10);
+		winmax = new JButton("max");
+		winmax.setSize(20, 10);
+		winmin = new JButton("min");
+		winmin.setSize(20, 10);
+		winex.addMouseListener(new MouseListener(){
+			@Override
+			public void mouseClicked(MouseEvent arg0) { System.exit(0);	}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mouseExited(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mousePressed(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) { /* NOTHING */ }		
+		});
+		winmin.addMouseListener(new MouseListener(){
+			@Override
+			public void mouseClicked(MouseEvent arg0) { /* NOTHING */ 	}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mouseExited(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mousePressed(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) { /* NOTHING */ }		
+		});
+		winmax.addMouseListener(new MouseListener(){
+			@Override
+			public void mouseClicked(MouseEvent arg0) { /* NOTHING */ 	}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mouseExited(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mousePressed(MouseEvent arg0) { /* NOTHING */ }
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) { /* NOTHING */ }		
+		});
+		box.add(winmin);
+		box.add(winmax);
+		box.add(winex);
 	}
 
 
