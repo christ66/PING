08d2a4bc1b9263f19e670a5a6acc64d9833e6894
river226 <river226@gmail.com>
2012-11-02 21:22:04 -0500

    Update Pane Option Button Classes

--- src/main/java/com/hackathon/ping/objects/PingRepoOption.java
+++ src/main/java/com/hackathon/ping/objects/PingRepoOption.java
@@ -1,7 +1,19 @@
 package com.hackathon.ping.objects;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public class PingRepoOption extends JButton {
+public class PingRepoOption extends PingButton {
+
+	public PingRepoOption(String text, ImageIcon imageIcon) {
+		super(text, imageIcon);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doMousePressed() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/com/hackathon/ping/objects/PingSourceOption.java
+++ src/main/java/com/hackathon/ping/objects/PingSourceOption.java
@@ -1,7 +1,19 @@
 package com.hackathon.ping.objects;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public class PingSourceOption extends JButton {
+public class PingSourceOption extends PingButton {
+
+	public PingSourceOption(String text, ImageIcon imageIcon) {
+		super(text, imageIcon);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doMousePressed() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
